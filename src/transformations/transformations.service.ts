ba28057979e4bc6ac0b5d823c52442995f82a109
Isac Gama <87917139+IsacGama@users.noreply.github.com>
2025-06-21 11:49:44 -0300

    feat: add get methods to transformations service

--- src/transformations/transformations.service.ts
+++ src/transformations/transformations.service.ts
@@ -1,4 +1,34 @@
+import { PrismaService } from 'src/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TransformationsService {}
+export class TransformationsService {
+  constructor(private prismaService: PrismaService) {}
+
+  async getAllTransformations(filters: {
+    name?: string;
+    characterId?: number;
+    limit?: number;
+  }) {
+    const { name, characterId, limit } = filters;
+    return await this.prismaService.transformation.findMany({
+      where: {
+        nome: name ? { contains: name, mode: 'insensitive' } : undefined,
+        personagemId: characterId !== undefined ? characterId : undefined,
+      },
+      take: limit ?? 10,
+      include: {
+        personagem: true,
+      },
+    });
+  }
+
+  async getTransformationById(id: number) {
+    return this.prismaService.transformation.findUnique({
+      where: { id },
+      include: {
+        personagem: true,
+      },
+    });
+  }
+}

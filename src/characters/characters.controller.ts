8d4a8ad253df31280a3c076e5d1d80a4ddc87704
Isac Gama <87917139+IsacGama@users.noreply.github.com>
2025-06-21 09:03:04 -0300

    feat: add create, update, and delete characters endpoints

--- src/characters/characters.controller.ts
+++ src/characters/characters.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CharactersService } from './characters.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('characters')
 export class CharactersController {
@@ -23,8 +33,41 @@ export class CharactersController {
   }
 
   @Get(':id')
-  async getCharacterById(id: number) {
-    const character = await this.charactersService.getCharacterById(id);
+  async getCharacterById(@Param('id') id: string) {
+    const character = await this.charactersService.getCharacterById(Number(id));
     return character;
+  }
+
+  @Post()
+  async createCharacter(
+    @Body()
+    body: {
+      nome: string;
+      idade: number;
+      ki?: string;
+      maxKi?: string;
+      raca: string;
+      genero: string;
+      descricao: string;
+      foto: string;
+      afiliacao: string;
+      sagaId: number;
+      planetaId: number;
+    },
+  ) {
+    return this.charactersService.createCharacter(body);
+  }
+
+  @Patch(':id')
+  async updateCharacter(
+    @Param('id') id: string,
+    @Body() data: Prisma.CharacterUpdateInput,
+  ) {
+    return this.charactersService.updateCharacter(Number(id), data);
+  }
+
+  @Delete(':id')
+  async deleteCharacter(@Param('id') id: string) {
+    return this.charactersService.deleteCharacter(Number(id));
   }
 }
--- src/characters/characters.service.ts
+++ src/characters/characters.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Character, Prisma } from '@prisma/client';
 
 @Injectable()
 export class CharactersService {
@@ -33,6 +34,48 @@ export class CharactersService {
 
   async getCharacterById(id: number) {
     return this.prismaService.character.findUnique({
+      where: { id },
+    });
+  }
+
+  async createCharacter(data: {
+    nome: string;
+    idade: number;
+    ki?: string;
+    maxKi?: string;
+    raca: string;
+    genero: string;
+    descricao: string;
+    foto: string;
+    afiliacao: string;
+    sagaId: number;
+    planetaId: number;
+  }): Promise<Character> {
+    return await this.prismaService.character.create({
+      data: {
+        nome: data.nome,
+        idade: data.idade,
+        ki: data.ki,
+        maxKi: data.maxKi,
+        raca: data.raca,
+        genero: data.genero,
+        descricao: data.descricao,
+        foto: data.foto,
+        afiliacao: data.afiliacao,
+        sagaId: data.sagaId,
+        planetaId: data.planetaId,
+      },
+    });
+  }
+  async updateCharacter(id: number, data: Prisma.CharacterUpdateInput) {
+    return await this.prismaService.character.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async deleteCharacter(id: number) {
+    return await this.prismaService.character.delete({
       where: { id },
     });
   }

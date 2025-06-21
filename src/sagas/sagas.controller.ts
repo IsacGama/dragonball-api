6fc7438c1e9510bbab2067bbf1a6a079195b89c6
Isac Gama <87917139+IsacGama@users.noreply.github.com>
2025-06-21 11:38:37 -0300

    feat: add get endpoints for sagas

--- src/sagas/sagas.controller.ts
+++ src/sagas/sagas.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { SagasService } from './sagas.service';
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller('sagas')
-export class SagasController {}
+export class SagasController {
+  constructor(private sagasService: SagasService) {}
+
+  @Get()
+  async getAllSagas(
+    @Query('name') name?: string,
+    @Query('personagens') personagens?: string,
+    @Query('limit') limit?: string,
+  ) {
+    return this.sagasService.getAllSagas({
+      name,
+      personagens,
+      limit: limit ? parseInt(limit) : undefined,
+    });
+  }
+
+  @Get(':id')
+  async getSagaById(@Query('id') id: string) {
+    const saga = await this.sagasService.getSagaById(Number(id));
+    return saga;
+  }
+}

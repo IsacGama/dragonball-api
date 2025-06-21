6c89a07d039baf8778a8bbe2d6bfa47d0dfa2148
Isac Gama <87917139+IsacGama@users.noreply.github.com>
2025-06-21 11:56:45 -0300

    feat: add post, patch, and delete endpoints for transformationsController

--- src/transformations/transformations.controller.ts
+++ src/transformations/transformations.controller.ts
@@ -1,5 +1,14 @@
 import { TransformationsService } from './transformations.service';
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('transformations')
 export class TransformationsController {
@@ -23,5 +32,29 @@ export class TransformationsController {
     const transformation =
       await this.transformationsService.getTransformationById(Number(id));
     return transformation;
+  }
+
+  @Post()
+  async createTransformation(
+    @Body()
+    body: {
+      nome: string;
+      descricao: string;
+      foto: string;
+      personagemId: number;
+    },
+  ) {
+    return this.transformationsService.createTransformation(body);
+  }
+  @Patch(':id')
+  async updateTransformation(
+    @Param('id') id: string,
+    @Body() data: { nome?: string; descricao?: string; foto?: string },
+  ) {
+    return this.transformationsService.updateTransformation(Number(id), data);
+  }
+  @Delete(':id')
+  async deleteTransformation(@Param('id') id: string) {
+    return this.transformationsService.deleteTransformation(Number(id));
   }
 }
